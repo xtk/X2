fae23d0b3fe225ddd456ecc968efa88e539edd1d
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2013-03-15 15:12:38 -0400

    Add the X.matrix and X.vector classes from XTK.

--- math/vector.js
+++ math/vector.js
@@ -1,17 +1,144 @@
-goog.provide('X.Vector');
+goog.provide('X.vector');
 
+// requires
 goog.require('goog.math.Vec3');
 
 
-goog.exportSymbol('X.Vector', goog.math.Vec3);
-goog.exportSymbol('X.Vector.prototype.clone', goog.math.Vec3.prototype.clone);
-goog.exportSymbol('X.Vector.prototype.magnitude',goog.math.Vec3.prototype.magnitude);
-goog.exportSymbol('X.Vector.prototype.scale',goog.math.Vec3.prototype.scale);
-goog.exportSymbol('X.Vector.prototype.invert',goog.math.Vec3.prototype.invert);
-goog.exportSymbol('X.Vector.prototype.normalize',goog.math.Vec3.prototype.normalize);
-goog.exportSymbol('X.Vector.prototype.add',goog.math.Vec3.prototype.add);
-goog.exportSymbol('X.Vector.prototype.subtract',goog.math.Vec3.prototype.subtract);
-goog.exportSymbol('X.Vector.dot',goog.math.Vec3.dot);
-goog.exportSymbol('X.Vector.cross',goog.math.Vec3.cross);
-goog.exportSymbol('X.Vector.distance',goog.math.Vec3.distance);
-goog.exportSymbol('X.Vector.lerp',goog.math.Vec3.lerp);
+// expose the following goog.math.Vec3 functionality
+/**
+ * @constructor
+ * @see goog.math.Vec3
+ */
+X.vector = goog.math.Vec3;
+
+
+/**
+ * @see goog.math.Vec3.prototype.clone
+ */
+X.vector.prototype.clone = goog.math.Vec3.prototype.clone;
+
+
+/**
+ * @see goog.math.Vec3.prototype.magnitude
+ */
+X.vector.prototype.magnitude = goog.math.Vec3.prototype.magnitude;
+
+
+/**
+ * @see goog.math.Vec3.prototype.scale
+ */
+X.vector.prototype.scale = goog.math.Vec3.prototype.scale;
+
+
+/**
+ * @see goog.math.Vec3.prototype.invert
+ */
+X.vector.prototype.invert = goog.math.Vec3.prototype.invert;
+
+
+/**
+ * @see goog.math.Vec3.prototype.add
+ */
+X.vector.prototype.add = goog.math.Vec3.prototype.add;
+
+
+/**
+ * @see goog.math.Vec3.prototype.subtract
+ */
+X.vector.prototype.subtract = goog.math.Vec3.prototype.subtract;
+
+
+/**
+ * Normalize the vector. The goog.math.Vec3.prototype.normalize
+ * did not check on a magnitude of 0 resulting in an error.
+ *
+ * @return {!X.vector|!goog.math.Vec3} The normalized vector.
+ */
+X.vector.prototype.normalize = function() {
+  // add a special check if the magnitude is 0
+  var _magnitude = this.magnitude();
+  if (_magnitude == 0) {
+    return this.scale(0);
+  }
+  return this.scale(1 / _magnitude);
+};
+
+
+/**
+ * @see goog.math.Vec3.dot
+ */
+X.vector.dot = goog.math.Vec3.dot;
+
+
+/**
+ * @see goog.math.Vec3.cross
+ */
+X.vector.cross = goog.math.Vec3.cross;
+
+
+/**
+ * @see goog.math.Vec3.distance
+ */
+X.vector.distance = goog.math.Vec3.distance;
+
+
+/**
+ * @see goog.math.Vec3.lerp
+ */
+X.vector.lerp = goog.math.Vec3.lerp;
+
+// now we need to make sure we can access the x,y,z
+// components of a goog.math.Vec3 which might be renamed
+// during the compilation. we don't want to modify the goog.math.Vec3
+// class so this is the easiest workaround.
+/**
+ * Get the x component of this vector.
+ *
+ * @return {number} The x component of this vector.
+ * @public
+ */
+X.vector.prototype.__defineGetter__('xx', function() {
+
+  return this.x;
+
+});
+
+
+/**
+ * Get the y component of this vector.
+ *
+ * @return {number} The y component of this vector.
+ * @public
+ */
+X.vector.prototype.__defineGetter__('yy', function() {
+
+  return this.y;
+
+});
+
+
+/**
+ * Get the z component of this vector.
+ *
+ * @return {number} The z component of this vector.
+ * @public
+ */
+X.vector.prototype.__defineGetter__('zz', function() {
+
+  return this.z;
+
+});
+
+
+goog.exportSymbol('X.vector', X.vector);
+goog.exportSymbol('X.vector.prototype.clone', X.vector.prototype.clone);
+goog.exportSymbol('X.vector.prototype.magnitude', X.vector.prototype.magnitude);
+goog.exportSymbol('X.vector.prototype.scale', X.vector.prototype.scale);
+goog.exportSymbol('X.vector.prototype.invert', X.vector.prototype.invert);
+goog.exportSymbol('X.vector.prototype.normalize', X.vector.prototype.normalize);
+goog.exportSymbol('X.vector.prototype.add', X.vector.prototype.add);
+goog.exportSymbol('X.vector.prototype.subtract', X.vector.prototype.subtract);
+goog.exportSymbol('X.vector.dot', X.vector.dot);
+goog.exportSymbol('X.vector.cross', X.vector.cross);
+goog.exportSymbol('X.vector.distance', X.vector.distance);
+goog.exportSymbol('X.vector.lerp', X.vector.lerp);
